56ab581c01690d6b272cac3521978b4cb451388f
awesome-edition-6075 <zachpardee@gmail.com>
2019-01-08 23:19:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function theBeatlesPlay(arrayMusicians, arrayInstruments){
   var emptyArray = [];
   
-  for (i = 0; i < arrayMusicians.length; i++) 
+  for (i = 0; i < arrayMusicians; i++) 
   { 
     return arrayMusicians[i] + " plays " + arrayInstruments[i];
   }
