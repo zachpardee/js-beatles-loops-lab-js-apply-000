3f20d68968ce995cafa6779fb0bec1241797e420
awesome-edition-6075 <zachpardee@gmail.com>
2019-01-08 23:45:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function johnLennonFacts(facts){
 function iLoveTheBeatles(number){
   var array = [];
   do {
-    array.push("I love the Beatles!")
+    array.push("I love the Beatles!");
     number++;
   }
   while(number < 15);
