4b63d1bd4ce39ba00b485ebf1e0df7fd89ecb984
awesome-edition-6075 <zachpardee@gmail.com>
2019-01-08 23:16:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-function theBeatlesPlay(arrayMusicians, arrayInstruments){
+function theBeatlesPlay(array, arrayInstruments){
   var emptyArray = [];
   
   for (i = 0; i < arrayMusicians.length; i++) 
